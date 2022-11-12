0791e551cb8c843973cdec8ccd1686106f18fb22
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-11-12 02:12:13 +0000

    feat: add jaen fields to ContentPage2

--- src/components/templates/WirGestaltenParty/WirGestaltenParty.tsx
+++ src/components/templates/WirGestaltenParty/WirGestaltenParty.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react"
+import { sliderDummyData } from "../../../constant/slider"
+import ContentPageSection from "../../ContentPageSection/ContentPageSection"
 import FourCard from "../../FourCard/FourCard"
+import NewsSlider from "../../NewsSlider/NewsSlider"
 import BallonGas from "./BallonGas"
 import ProductCatlog from "./ProductCatlog"
 import WirGestaltenPartyHero from "./WirGestaltenPartyHero"
@@ -7,41 +10,16 @@ import WirGestaltenPartyHero from "./WirGestaltenPartyHero"
 interface IWirGestaltenPartyProps {}
 
 const WirGestaltenParty: FC<IWirGestaltenPartyProps> = () => {
-  const cards = [
-    {
-      title: "Geburtstage",
-      image: "/images/we_design_party/card1.png",
-    },
-    {
-      title: "Baby & Geburt",
-      image: "/images/we_design_party/card2.png",
-    },
-    {
-      title: "Großhandel",
-      image: "/images/we_design_party/card3.png",
-    },
-    {
-      title: "Partyartikel",
-      image: "/images/we_design_party/card4.png",
-    },
-  ]
-  const cardsMobile = [
-    {
-      title: "Hochzeitsballons",
-      image: "/images/decorationen/four_cards/card1.png",
-    },
-    {
-      title: "Hochzeitsballons",
-      image: "/images/decorationen/four_cards/card1.png",
-    },
-  ]
-
   return (
     <>
       <WirGestaltenPartyHero />
       <ProductCatlog />
-      <FourCard cardsMobileData={cardsMobile} cardsData={cards} />
-      <BallonGas />
+      <ContentPageSection />
+      <NewsSlider
+        withoutImageVariant
+        showNewsTitle={true}
+        slides={sliderDummyData}
+      />
     </>
   )
 }
