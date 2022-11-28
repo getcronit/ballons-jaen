00ab79935cac226b61c15c6e0334334062f3b5fd
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-11-28 00:05:38 +0000

    fix: contentpage image sizing issue

--- src/components/ContentPageSection/ContentPageSection.tsx
+++ src/components/ContentPageSection/ContentPageSection.tsx
@@ -267,20 +267,20 @@ const FullWidthImageSection = connectSection(
         <Box
           my={{ base: "4 !important", md: "12 !important" }}
           borderRadius={{ base: ".625rem", md: "2rem" }}
-          minH={{
-            base: "11.25rem",
-            md: "18.75rem",
-            lg: "25rem",
-            xl: "29.375rem",
-          }}
+          // minH={{
+          //   base: "11.25rem",
+          //   md: "18.75rem",
+          //   lg: "25rem",
+          //   xl: "29.375rem",
+          // }}
+          
           boxShadow="light"
           overflow={"hidden"}
         >
           <Field.Image
-            style={{ minHeight: "inherit" }}
             name="image"
             defaultValue={undefined}
-            objectFit="cover"
+            // objectFit="cover"
           />
         </Box>
       </Container>
