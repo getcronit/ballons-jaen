7a961d16b92364901eb8b547776bc35adcb3d857
Nico Schett <52858351+schettn@users.noreply.github.com>
2023-03-27 15:07:31 +0000

    WIP: add maps

--- src/components/molecules/GoogleMaps/GoogleMaps.tsx
+++ src/components/molecules/GoogleMaps/GoogleMaps.tsx
@@ -1,4 +1,6 @@
-import {BoxProps} from '@chakra-ui/react'
+import {Box, BoxProps, Heading, Link, Text} from '@chakra-ui/react'
+import {getCookieConsentApi} from '@snek-at/jaen'
+import {useEffect, useState} from 'react'
 // import {useCookieConsent} from '@jaenjs/jaen'
 
 export interface GoogleMapsProps extends BoxProps {
@@ -6,41 +8,52 @@ export interface GoogleMapsProps extends BoxProps {
 }
 
 export const GoogleMaps = ({src, ...props}: GoogleMapsProps) => {
-  return <>Todo</>
+  const handleAccept = () => {
+    const CookieConsentApi = getCookieConsentApi()
 
-  // const {consentLevel, cookieconsent} = useCookieConsent()
+    CookieConsentApi.accept('analytics')
 
-  // const handleAccept = () => {
-  //   cookieconsent.accept(['analytics'])
-  // }
+    setMapsEnabled(true)
+  }
 
-  // if (!consentLevel.includes('analytics')) {
-  //   return (
-  //     <Box bg="gray.200">
-  //       <Heading as="h1" size="xl">
-  //         Google Maps is nicht verfügbar
-  //       </Heading>
-  //       <Text as="h2" size="lg">
-  //         Durch Ihre Cookie Einstellungen können wir Google Maps nicht anzeigen.
-  //       </Text>
-  //       <Text as="h2" size="lg">
-  //         Bitte aktivieren Sie Cookies, um Google Maps anzuzeigen.{' '}
-  //         <Link onClick={handleAccept}>Analytics Cookie aktivieren</Link>
-  //       </Text>
-  //     </Box>
-  //   )
+  const [mapsEnabled, setMapsEnabled] = useState(false)
 
-  // return (
-  //   <Box {...props}>
-  //     <iframe
-  //       src={src}
-  //       width="100%"
-  //       height="600"
-  //       style={{
-  //         border: 1
-  //       }}
-  //       loading="lazy"
-  //     />
-  //   </Box>
-  // )
+  useEffect(() => {
+    const CookieConsentApi = getCookieConsentApi()
+
+    const analyticsEnabled = CookieConsentApi.allowedCategory('analytics')
+
+    setMapsEnabled(analyticsEnabled)
+  }, [])
+
+  if (!mapsEnabled) {
+    return (
+      <Box bg="gray.200">
+        <Heading as="h1" size="xl">
+          Google Maps is nicht verfügbar
+        </Heading>
+        <Text as="h2" size="lg">
+          Durch Ihre Cookie Einstellungen können wir Google Maps nicht anzeigen.
+        </Text>
+        <Text as="h2" size="lg">
+          Bitte aktivieren Sie Cookies, um Google Maps anzuzeigen.{' '}
+          <Link onClick={handleAccept}>Analytics Cookie aktivieren</Link>
+        </Text>
+      </Box>
+    )
+  }
+
+  return (
+    <Box {...props}>
+      <iframe
+        src={src}
+        width="100%"
+        height="600"
+        style={{
+          border: 1
+        }}
+        loading="lazy"
+      />
+    </Box>
+  )
 }
