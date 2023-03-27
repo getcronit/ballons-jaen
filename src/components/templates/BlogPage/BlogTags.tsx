12713e6fdae3fdae99c7f8e3beff0185c0b36846
Nico Schett <52858351+schettn@users.noreply.github.com>
2023-03-27 15:51:26 +0000

    WIP: reduce minH of BlogTags section

--- src/components/templates/BlogPage/BlogTags.tsx
+++ src/components/templates/BlogPage/BlogTags.tsx
@@ -5,7 +5,7 @@ const BlogTagItem = connectBlock(
   () => {
     return (
       <Tag variant="normal">
-        {<Field.Text name="tagText" label="Text" defaultValue="Tag" />}
+        <Field.Text name="tagText" label="Text" defaultValue="Tag" />
       </Tag>
     )
   },
@@ -22,6 +22,10 @@ export const BlogTags: React.FC<{
     <Box minW="24">
       <Field.Section
         as={HStack}
+        props={{
+          minH: '24px',
+          spacing: '2'
+        }}
         name={props.fieldName}
         label="Tags"
         blocks={[BlogTagItem]}
