636e0e9830eee669acc6b8702b8743e5bbac84d9
Florian Kleber <kleber@snek.at>
2023-04-12 09:50:29 +0000

    feat: adjust hero ballons

--- src/components/molecules/ParallaxBackground/ParallaxBackground.tsx
+++ src/components/molecules/ParallaxBackground/ParallaxBackground.tsx
@@ -28,46 +28,46 @@ export const ParallaxBackground = ({
       css={style.Section(strokeColor, backgroundColor, noScroll)}
       ref={ref}
       backgroundColor={backgroundColor}>
-      <Box className="parallax__layer parallax__layer__0" pl="10%" pt="20%">
+      <Box className="parallax__layer parallax__layer__0" pl="80%" pt="20%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__1" pl="60%" pt="30%">
+      <Box className="parallax__layer parallax__layer__1" pl="60%" pt="30%" display={{ base: 'none', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__2" pl="30%" pt="90%">
+      <Box className="parallax__layer parallax__layer__2" pl="30%" pt="90%" display={{ base: 'block', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__3" pl="50%" pt="15%">
+      <Box className="parallax__layer parallax__layer__3" pl="50%" pt="15%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__4" pl="40%" pt="30%">
+      <Box className="parallax__layer parallax__layer__4" pl="40%" pt="30%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__5" pl="80%" pt="80%">
+      <Box className="parallax__layer parallax__layer__5" pl="80%" pt="80%" display={{ base: 'block', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__6" pl="1%" pt="40%">
+      <Box className="parallax__layer parallax__layer__6" pl="1%" pt="40%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__7" pl="50%" pt="50%">
+      <Box className="parallax__layer parallax__layer__7" pl="50%" pt="50%" display={{ base: 'none', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__8" pl="20%" pt="30%">
+      <Box className="parallax__layer parallax__layer__8" pl="60%" pt="30%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__9" pl="50%" pt="80%">
+      <Box className="parallax__layer parallax__layer__9" pl="50%" pt="80%" display={{ base: 'block', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__10" pl="80%" pt="70%">
+      <Box className="parallax__layer parallax__layer__10" pl="80%" pt="70%" display={{ base: 'block', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__11" pl="30%" pt="50%">
+      <Box className="parallax__layer parallax__layer__11" pl="60%" pt="50%" display={{ base: 'none', lg: 'none', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__12" pl="70%" pt="30%">
+      <Box className="parallax__layer parallax__layer__12" pl="70%" pt="30%" display={{ base: 'none', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
-      <Box className="parallax__layer parallax__layer__13" pl="90%" pt="60%">
+      <Box className="parallax__layer parallax__layer__13" pl="90%" pt="60%" display={{ base: 'none', lg: 'block', xl: 'block' }}>
         <Ballon className="background-Ballon" />
       </Box>
       <div className="parallax__cover"></div>
