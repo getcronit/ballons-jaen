8d3027a8e9c3828b01a0a22479b40d5dfa366520
Florian Kleber <kleber@snek.at>
2023-05-22 18:44:40 +0000

    feat: add textDecoration on active nav link

--- src/components/Navigation/NavLinks.tsx
+++ src/components/Navigation/NavLinks.tsx
@@ -1,4 +1,4 @@
-import {EditIcon} from '@chakra-ui/icons'
+import { EditIcon } from '@chakra-ui/icons'
 import {
   Box,
   IconButton,
@@ -12,20 +12,59 @@ import {
   StackProps,
   useDisclosure
 } from '@chakra-ui/react'
-import {Link, navigate} from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import React from 'react'
 
-import {MarkdownLinksForm} from './BottomNav'
-import {useJaenNavTop, useJaenNavBottom} from './useJaenNavigation'
+import { MarkdownLinksForm } from './BottomNav'
+import { useJaenNavTop, useJaenNavBottom } from './useJaenNavigation'
+
+const findBestMatch = (path: string, paths: Array<string>) => {
+  let bestMatch: string | undefined
+  let bestMatchScore = 0
+
+  // check how many a path matches the current path
+  // if it is the best match, save it
+  paths.forEach(pathToMatch => {
+
+    if (pathToMatch !== "/") {
+      // iterate over all path parts and check how many of them match
+      let score = 0
+      const pathParts = path.replace(/\/$/, '').split('/').filter(Boolean)
+      const pathToMatchParts = pathToMatch
+        .replace(/\/$/, '')
+        .split('/')
+        .filter(Boolean)
+      for (let i = 0; i < pathParts.length; i++) {
+        if (pathParts[i] !== pathToMatchParts[i]) {
+          // if the path part does not match, exit the loop
+          break
+        }
+
+        score++
+      }
+
+      // if the score is better than the current best match, save it
+      if (score > bestMatchScore) {
+        bestMatch = pathToMatch
+        bestMatchScore = score
+      }
+    } else {
+      bestMatch = pathToMatch
+      bestMatchScore = 1
+    }
+  })
+
+  return bestMatch
+}
 
 export const TopNavLinks: React.FC<
   StackProps & {
     childrenTextAlign?: 'left' | 'center' | 'right'
   }
-> = ({childrenTextAlign, ...props}) => {
-  const {isEditing, navLinks, markdown, updateNavigation} = useJaenNavTop()
+> = ({ childrenTextAlign, ...props }) => {
+  const { isEditing, navLinks, markdown, updateNavigation } = useJaenNavTop()
 
-  const {onOpen, onClose, isOpen} = useDisclosure()
+  const { onOpen, onClose, isOpen } = useDisclosure()
   const firstFieldRef = React.useRef(null)
 
   return (
@@ -60,7 +99,7 @@ export const TopNavLinks: React.FC<
               _hover={{
                 textDecoration: 'underline'
               }}
-              fontSize={{base: 'sm', lg: '1rem'}}
+              fontSize={{ base: 'sm', lg: '1rem' }}
               transition="0.2s ease-in"
               color="brand.dark_gray">
               {link.label}
@@ -106,11 +145,18 @@ export const BottomNavLinks: React.FC<
   StackProps & {
     childrenTextAlign?: 'left' | 'center' | 'right'
   }
-> = ({childrenTextAlign, ...props}) => {
-  const {isEditing, navLinks, markdown, updateNavigation} = useJaenNavBottom()
+> = ({ childrenTextAlign, ...props }) => {
+  const { isEditing, navLinks, markdown, updateNavigation } = useJaenNavBottom()
 
-  const {onOpen, onClose, isOpen} = useDisclosure()
+  const { onOpen, onClose, isOpen } = useDisclosure()
   const firstFieldRef = React.useRef(null)
+  const bestMatch = findBestMatch(
+    window.location.pathname,
+    navLinks.map(l => l.to)
+  )
+  console.log(bestMatch)
+  console.log(window.location.pathname)
+  console.log(navLinks.map(l => l.to))
 
   return (
     <>
@@ -118,6 +164,9 @@ export const BottomNavLinks: React.FC<
         {navLinks.map((link, index) => {
           return (
             <CLink
+              textDecoration={link.to === bestMatch
+                ? 'underline'
+                : 'none'}
               _before={{
                 display: 'block',
                 content: `"${link.label}"`,
@@ -144,7 +193,7 @@ export const BottomNavLinks: React.FC<
               _hover={{
                 textDecoration: 'underline'
               }}
-              fontSize={{md: 'md', lg: '1.375rem', xl: '1.4rem', '2xl': 'lg'}}
+              fontSize={{ md: 'md', lg: '1.375rem', xl: '1.4rem', '2xl': 'lg' }}
               transition="0.2s ease-in"
               color="brand.dark_gray">
               {link.label}
