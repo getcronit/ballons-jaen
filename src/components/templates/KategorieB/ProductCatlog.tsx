5c81f0f7d56f6b7b3b7a0eb5d69d6851f2a16339
Florian Kleber <kleber@snek.at>
2023-05-25 08:49:40 +0000

    feat: combine text field with Chakra at KategorieB

--- src/components/templates/KategorieB/ProductCatlog.tsx
+++ src/components/templates/KategorieB/ProductCatlog.tsx
@@ -1,22 +1,22 @@
-import {Box, Container, Flex, Heading, Stack, Text} from '@chakra-ui/react'
-import {Field} from '@snek-at/jaen'
-import {FC} from 'react'
-import {CONTAINER_MAX_WIDTH} from '../../../constant/sizes'
+import { Box, Container, Flex, Heading, Stack, Text } from '@chakra-ui/react'
+import { Field } from '@snek-at/jaen'
+import { FC } from 'react'
+import { CONTAINER_MAX_WIDTH } from '../../../constant/sizes'
 import LinkButtonField from '../../fields/LinkButtonField'
 
-interface IProductCatlogProps {}
+interface IProductCatlogProps { }
 
 const ProductCatlog: FC<IProductCatlogProps> = () => {
   return (
     <Container
       maxW={CONTAINER_MAX_WIDTH}
       pos="relative"
-      top={{base: '-3rem', md: '-6.25rem', lg: '-12.5rem'}}
-      mb={{base: '1rem', md: '-4.25rem', lg: '-10.5rem'}}>
-      <Flex gap="12" align="center" flexDir={{base: 'column', lg: 'row'}}>
+      top={{ base: '-3rem', md: '-6.25rem', lg: '-12.5rem' }}
+      mb={{ base: '1rem', md: '-4.25rem', lg: '-10.5rem' }}>
+      <Flex gap="12" align="center" flexDir={{ base: 'column', lg: 'row' }}>
         <Box
           flex="1"
-          w={{base: '70%'}}
+          w={{ base: '70%' }}
           h={{
             base: '30rem',
             sm: '31.25rem',
@@ -25,7 +25,7 @@ const ProductCatlog: FC<IProductCatlogProps> = () => {
           }}>
           <Field.Image name="stack_cards" objectFit="contain" />
         </Box>
-        <Box display={{base: 'block', md: 'none'}}>
+        <Box display={{ base: 'block', md: 'none' }}>
           <LinkButtonField
             name="catalogueButton"
             defaultValue={'Zum Katalog'}
@@ -34,43 +34,37 @@ const ProductCatlog: FC<IProductCatlogProps> = () => {
           />
         </Box>
         <Stack
-          display={{base: 'none', md: 'flex'}}
+          display={{ base: 'none', md: 'flex' }}
           flex="1"
-          pl={{base: '4rem', xl: '9rem'}}
-          pb={{base: '2rem', xl: '5rem'}}
+          pl={{ base: '4rem', xl: '9rem' }}
+          pb={{ base: '2rem', xl: '5rem' }}
           pt="3.125rem"
           pr="1.25rem"
           bg="white"
           boxShadow="light"
           borderRadius="xl">
-          <Heading
-            fontSize={{base: 'md', md: 'lg', xl: 'xl'}}
-            fontWeight="semibold">
-            <Field.Text
-              name="cataloguePreTitle"
-              label="Titel"
-              defaultValue={'Katalog'}
-            />
-          </Heading>
-          <Heading
-            fontSize={{base: 'md', md: 'xl', lg: '2xl', xl: '3xl'}}
-            fontWeight="semibold">
-            <Field.Text
-              name="catalogueTitle"
-              label="Titel"
-              rtf
-              defaultValue="<p>Unsere <i>Kataloge</i></p>"
-            />
-          </Heading>
-          <Text fontSize={{base: 'sm', lg: 'md'}} as="span">
-            <Field.Text
-              name="catalogueText"
-              label="Text"
-              defaultValue={
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam, atque iusto culpa libero nostrum sit fuga cumque sunt tenetur! Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-              }
-            />
-          </Text>
+          <Field.Text
+            as={Heading}
+            fontSize={{ base: 'md', md: 'lg', xl: 'xl' }}
+            fontWeight="semibold"
+            name="cataloguePreTitle"
+            defaultValue={'Katalog'}
+          />
+          <Field.RichText
+            as={Heading}
+            fontSize={{ base: 'md', md: 'xl', lg: '2xl', xl: '3xl' }}
+            fontWeight="semibold"
+            name="catalogueTitle"
+            defaultValue="<p>Unsere <i>Kataloge</i></p>"
+          />
+          <Field.Text
+            fontSize={{ base: 'sm', lg: 'md' }}
+            name="catalogueText"
+
+            defaultValue={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam, atque iusto culpa libero nostrum sit fuga cumque sunt tenetur! Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+            }
+          />
           <Box>
             <LinkButtonField
               name="catalogueButton"
--- src/components/templates/KategorieB/WirGestaltenPartyHero.tsx
+++ src/components/templates/KategorieB/WirGestaltenPartyHero.tsx
@@ -8,19 +8,19 @@ import {
   Stack,
   Text
 } from '@chakra-ui/react'
-import {Field} from '@snek-at/jaen'
-import {FC} from 'react'
-import {CONTAINER_MAX_WIDTH} from '../../../constant/sizes'
+import { Field } from '@snek-at/jaen'
+import { FC } from 'react'
+import { CONTAINER_MAX_WIDTH } from '../../../constant/sizes'
 import LinkButtonField from '../../fields/LinkButtonField'
 
-interface IWirGestaltenPartyHeroProps {}
+interface IWirGestaltenPartyHeroProps { }
 
 const WirGestaltenPartyHero: FC<IWirGestaltenPartyHeroProps> = () => {
   return (
     <>
       <Container maxW={CONTAINER_MAX_WIDTH}>
         <Flex
-          align={{base: 'start', md: 'center'}}
+          align={{ base: 'start', md: 'center' }}
           pos="relative"
           h={{
             base: '25rem',
@@ -32,56 +32,50 @@ const WirGestaltenPartyHero: FC<IWirGestaltenPartyHeroProps> = () => {
           <Image
             pos="absolute"
             top="0"
-            left={{base: '-20%', md: '-40%'}}
-            h={{base: '-20%', md: 'full'}}
+            left={{ base: '-20%', md: '-40%' }}
+            h={{ base: '-20%', md: 'full' }}
             src="/images/we_design_party/shape.svg"
           />
-          <Stack pos="relative" w="100%" top={{base: '4rem', md: 'unset'}}>
-            <Heading
-              fontSize={{base: 'md', md: '2xl', lg: '3xl', xl: '4xl'}}
+          <Stack pos="relative" w="100%" top={{ base: '4rem', md: 'unset' }}>
+
+            <Field.RichText
+              as={Heading}
+              fontSize={{ base: 'md', md: '2xl', lg: '3xl', xl: '4xl' }}
               fontWeight="semibold"
               sx={{
                 'i, em': {
                   fontSize: '1.5em'
                 }
-              }}>
-              <Field.Text
-                name="title"
-                label="Title"
-                rtf
-                defaultValue="<p>Wir gestalten </br> <i>Ihre Party</i></p>"
-              />
-            </Heading>
+              }}
+              name="title"
+              defaultValue="<p>Wir gestalten </br> <i>Ihre Party</i></p>"
+            />
 
-            <Text
-              display={{base: 'none', sm: 'block'}}
-              fontSize={{base: 'sm', lg: 'md'}}
-              maxW={{md: '70%'}}
-              as="span">
-              <Field.Text
-                name="text"
-                label="Text"
-                defaultValue={
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper'
-                }
-              />
-            </Text>
+            <Field.Text
+              display={{ base: 'none', sm: 'block' }}
+              fontSize={{ base: 'sm', lg: 'md' }}
+              maxW={{ md: '70%' }}
+              name="text"
+              defaultValue={
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper'
+              }
+            />
             <LinkButtonField
               name="cta"
-              size={{base: 'xs', md: 'lg'}}
-              mt={{base: '8 !important', md: '4'}}
+              size={{ base: 'xs', md: 'lg' }}
+              mt={{ base: '8 !important', md: '4' }}
             />
           </Stack>
           <Grid
             placeItems="center"
             pos="relative"
-            top={{base: '1.25rem', md: 'unset'}}>
+            top={{ base: '1.25rem', md: 'unset' }}>
             <Box
               borderRadius="full"
               boxShadow="light"
               // w={{ base: "17.5rem", sm: "auto " }}
-              w={{base: '30vh', md: '40vh'}}
-              h={{base: '30vh', md: '40vh'}}>
+              w={{ base: '30vh', md: '40vh' }}
+              h={{ base: '30vh', md: '40vh' }}>
               <Field.Image name="heroImage" />
             </Box>
           </Grid>
