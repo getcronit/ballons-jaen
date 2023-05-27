986432b72408ea34e1bc582a7db952231721c173
Florian Kleber <kleber@snek.at>
2023-05-27 14:17:38 +0000

    WIP: combine Contact and AboutUs page

---
+++ src/components/templates/ContactAboutUs/ContactAboutUs.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import AboutBallons from '../AboutUs/AboutBallons';
+import AboutHeroSection from '../AboutUs/AboutHeroSection';
+import OurCustomers from '../AboutUs/OurCustomers';
+import OurTeam from '../AboutUs/OurTeam/OurTeam';
+import Contact from '../Contact/Contact';
+import ContactBottomSection from '../Contact/ContactBottomSection';
+
+interface IAboutUsProps {}
+
+const ContactAboutUs: FC<IAboutUsProps> = ({}) => {
+  return (
+    <>
+      {/* <AboutHeroSection /> */}
+      <Contact />
+      {/* <AboutBallons /> */}
+      {/* <OurCustomers /> */}
+      <OurTeam />
+      <ContactBottomSection />
+    </>
+  );
+};
+export default ContactAboutUs;
